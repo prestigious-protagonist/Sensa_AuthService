38f8dbf7b9d91702cc5070b1dca03d33eddaf95f
prestigious-protagonist <jaskaranyt123@gmail.com>
2025-04-02 15:43:25 +0530

    pushing authservice

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const { PORT, DB_SYNC } = require("./config/serverConfig");
+const { sequelize } = require("./models/index");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// API Routes
+const apiRouter = require("./routes/index");
+app.use("/authService/api", apiRouter); // All routes start from "/api"
+
+// Basic route for testing
+app.get("/authService/api/v1/home", (req, res) => {
+  res.send("Hello from Auth Service");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Auth Service running on port: ${PORT}`);
+});
+
+module.exports = app;
